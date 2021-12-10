5ce20e083752b7fb3ed50855e63ce9e050a67ed2
Vladimir Sokolov <vsokol74@main.ru>
2021-12-10 18:53:01 +0300

    Homework for lesson 6

---
+++ lesson-6/bookparser/runner.py
@@ -0,0 +1,13 @@
+from scrapy.crawler import CrawlerProcess
+from scrapy.settings import Settings
+from bookparser import settings
+from bookparser.spiders.labirint import LabirintSpider
+
+if __name__ == '__main__':
+    crawler_settings = Settings()
+    crawler_settings.setmodule(settings)
+
+    process = CrawlerProcess(settings=crawler_settings)
+    process.crawl(LabirintSpider)
+
+    process.start()
